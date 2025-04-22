1db9bcaf93653e21aff83e45a5cc6db29d9fe913
pangwoo96 <junggw96@gmail.com>
2025-04-23 01:15:52 +0900

    chore: git 초기 세팅

---
+++ src/main/java/com/smallsquare/SmallsquareApplication.java
@@ -0,0 +1,13 @@
+package com.smallsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallsquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallsquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smallsquare/SmallsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallsquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
