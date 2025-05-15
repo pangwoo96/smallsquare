284ee1dbac915ad09d515a310be3c24a3e3e750f
pangwoo96 <junggw96@gmail.com>
2025-05-15 14:21:12 +0900

    refactor: QueryDSl VO 수정으로 생성자 추가

--- src/main/java/com/smallsquare/modules/user/web/dto/response/UserInfoResDto.java
+++ src/main/java/com/smallsquare/modules/user/web/dto/response/UserInfoResDto.java
@@ -1,11 +1,22 @@
 package com.smallsquare.modules.user.web.dto.response;
 
+import com.smallsquare.modules.user.domain.vo.Email;
+import com.smallsquare.modules.user.domain.vo.Name;
+import com.smallsquare.modules.user.domain.vo.Nickname;
+import com.smallsquare.modules.user.domain.vo.Username;
 import lombok.*;
 
 @Getter @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoResDto {
+
+    public UserInfoResDto(Username username, Nickname nickname, Email email, Name name) {
+        this.username = username.getUsername();
+        this.nickname = nickname.getNickname();
+        this.email = email.getEmail();
+        this.name = name.getName();
+    }
 
     private String username;
 
