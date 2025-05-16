68cca9a41653c732ee6f52b841af436320fa2402
pangwoo96 <junggw96@gmail.com>
2025-05-16 18:31:39 +0900

    feat: Post 생성 기능 구현 - Controller, ReqDto - Service, Repository

---
+++ src/main/java/com/smallsquare/modules/post/infrastructure/repository/PostRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.smallsquare.modules.post.infrastructure.repository;
+
+import com.smallsquare.modules.post.domain.entity.Post;
+import com.smallsquare.modules.post.domain.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepositoryImpl implements PostRepository {
+
+    private final JpaPostRepository jpaPostRepository;
+
+    @Override
+    public Post save(Post post) {
+        return jpaPostRepository.save(post);
+    }
+}
